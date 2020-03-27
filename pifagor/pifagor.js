7753cf8266f124c7dfbd86637a769c180b79c09d
ValeriiaRohozhyna <rohozhyna.valeriia@gmail.com>
2020-03-27 19:26:24 +0200

    the first commit

---
+++ pifagor/pifagor.js
@@ -0,0 +1,21 @@
+var main = document.getElementById("main");
+
+// Pifagor's table generator
+for (var i = 1; i <= 9; i++) {
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (var j = 1; j <= 9; j++) {
+        var cell = document.createElement("div");
+        cell.setAttribute("class", "cell cell-main");
+        cell.innerText = i * j;
+        row.appendChild(cell);
+        if (j > i) {
+            cell.classList.add("cell-undermain")
+        };
+        if (i > j) {
+            cell.classList.add("cell-overmain")
+        };
+    }
+    main.appendChild(row);
+    
+}
