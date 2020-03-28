ce3c5c7db3d5d88da20bd57a5f1cd0c62d2d3c09
ValeriiaRohozhyna <rohozhyna.valeriia@gmail.com>
2020-03-28 21:04:39 +0200

    last changes

---
+++ min and max array's elements/main.js
@@ -0,0 +1,24 @@
+var someArray = [10, 15, 14, 16, -15, -22, 11, 18, 14, 36]
+
+function calcMinElem(array) {
+    var minElem = array[0];
+    for (i = 1; i < array.length; i++) {
+        if (array[i] < minElem) {
+            minElem = array[i];
+        }
+    }
+    return minElem;
+}
+
+console.log("The minElem is: ", calcMinElem(someArray));
+
+function calcMaxElem(array) {
+    var maxElem = array[0];
+    for(i = 1; i < array.length; i++) {
+        if(array[i] > maxElem) {
+            maxElem = array[i];
+        }
+    }
+    return maxElem;
+}
+console.log("The maxElem is: ", calcMaxElem(someArray));
