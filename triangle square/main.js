215ae0dbf521ecb6980ef44dc320609aa72da971
ValeriiaRohozhyna <rohozhyna.valeriia@gmail.com>
2020-03-28 13:31:45 +0200

    last changes

--- triangle square/main.js
+++ triangle square/main.js
@@ -1,9 +1,8 @@
 
 function calcTriangleSquare (a, b, c) {
     var p;
-    p = (a + b + c) / 2;
-    return Math.round(Math.sqrt(p * (p - a) + (p - b) + (p - c)));
-}
+    p = (a + b + c) / 2; //perimetr
+    return Math.round(Math.sqrt(p * (p - a) + (p - b) + (p - c))); //square
+};
 
-;
-console.log("Triangle Square is about", calcTriangleSquare(8, 25, 16), "m2");
+console.log("Triangle Square is about", calcTriangleSquare(8, 25, 16), "m2"); 
