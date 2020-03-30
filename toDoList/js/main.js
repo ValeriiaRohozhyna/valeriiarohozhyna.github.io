dc03f6408884e06d7477b138767203eceff9e51c
ValeriiaRohozhyna <rohozhyna.valeriia@gmail.com>
2020-03-30 17:58:27 +0300

    last changes: icon was added

---
+++ toDoList/js/main.js
@@ -0,0 +1,38 @@
+btnAdd.addEventListener("click", addToDoItem, false);
+
+var toDoList = [];
+
+function addToDoItem(e) {
+    if (whatToDo.value.length < 1) {
+        return;
+    }
+    if (!toDoList.includes(whatToDo.value)) {
+        var toDoItem = document.createElement("li");
+
+        toDoItem.innerText = whatToDo.value;
+        list.appendChild(toDoItem);
+
+        toDoItem.addEventListener("click", toDoItemDone, false);
+        function toDoItemDone(e) {
+            if (e.target.style.textDecoration == "line-through") {
+                e.target.style.textDecoration = "";
+            } else {
+                e.target.style.textDecoration = "line-through";
+            }
+        }
+        
+        var toDoSpan = document.createElement("span");
+        toDoSpan.setAttribute("class", "fa fa-trash");
+        toDoItem.appendChild(toDoSpan);
+
+        toDoSpan.addEventListener("click", toDoItemRemove, false);
+        function toDoItemRemove(e) {
+            if (confirm("Are you sure I need to delete this item?")) {
+                toDoItem.remove();
+            }
+        }
+
+        toDoList.push(whatToDo.value);
+        whatToDo.value = "";
+    }
+}
