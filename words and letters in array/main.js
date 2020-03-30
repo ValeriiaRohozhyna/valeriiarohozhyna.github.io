bafddaa298cd3a0f665835194ba86b5ac324d974
ValeriiaRohozhyna <rohozhyna.valeriia@gmail.com>
2020-03-30 14:09:19 +0300

    added calcEachLetter

--- words and letters in array/main.js
+++ words and letters in array/main.js
@@ -1,4 +1,4 @@
-var someArray = ["This", "is", "one", "of",  "my", "arrays"];
+var someArray = ["This", "is", "one", "of", "my", "arrays"];
 
 function calcWords(array) {
     return array.length; // number of words
@@ -10,3 +10,31 @@ function calcLetters(array) {
 }
 console.log("There are", calcLetters(someArray), "letters here");
 
+function calcEachLetter(array) {
+    var arrayToString = array.join("");
+    var resultArray = {};
+    for (i = 0; i < arrayToString.length; i++) {
+        if (resultArray.hasOwnProperty(arrayToString[i])) {
+            resultArray[arrayToString[i]]++;
+        } else {
+            resultArray[arrayToString[i]] = 1;
+        }
+    }
+    return resultArray;
+}
+
+console.log(calcEachLetter(someArray)); 
+/*
+T: 1
+h: 1
+i: 2
+s: 3
+o: 2
+n: 1
+e: 1
+f: 1
+m: 1
+y: 2
+a: 2
+r: 2
+*/
