36c08adab452ef3290f395133ae2687e82804bfd
ValeriiaRohozhyna <rohozhyna.valeriia@gmail.com>
2020-03-31 18:39:11 +0300

    last changes

--- toDoList/js/main.js
+++ toDoList/js/main.js
@@ -1,10 +1,11 @@
 btnAdd.addEventListener("click", addToDoItem, false);
-whatToDo.addEventListener("keydown", addToDoItem, false )
+whatToDo.addEventListener("keydown", addToDoItem, false)
+
 
 var toDoList = [];
 
 function addToDoItem(e) {
-    if((e.type == "keydown") && (e.keyCode != 13)) {
+    if ((e.type == "keydown") && (e.keyCode != 13)) {
         return;
     }
     if (whatToDo.value.length < 1) {
@@ -12,11 +13,11 @@ function addToDoItem(e) {
     }
     if (!toDoList.includes(whatToDo.value)) {
         var toDoItem = document.createElement("li");
-
         toDoItem.innerText = whatToDo.value;
-        list.appendChild(toDoItem);
+        list.appendChild(toDoItem); // to add the "li" into "ul"
 
         toDoItem.addEventListener("click", toDoItemDone, false);
+        //to make toDoItem done
         function toDoItemDone(e) {
             if (e.target.style.textDecoration == "line-through") {
                 e.target.style.textDecoration = "";
@@ -24,18 +25,21 @@ function addToDoItem(e) {
                 e.target.style.textDecoration = "line-through";
             }
         }
-        
+        // to add an icon to the "li" element
         var toDoSpan = document.createElement("span");
         toDoSpan.setAttribute("class", "fa fa-trash");
         toDoItem.appendChild(toDoSpan);
 
         toDoSpan.addEventListener("click", toDoItemRemove, false);
+
+        //to remove the toDoItem
         function toDoItemRemove(e) {
-            if (confirm("Are you sure I need to delete this item?")) {
+            if (confirm("Are you sure you want to delete this item?")) {
                 toDoItem.remove();
             }
-        }
+        };
 
+        //to add the value of input to the array toDoList
         toDoList.push(whatToDo.value);
         whatToDo.value = "";
     }
