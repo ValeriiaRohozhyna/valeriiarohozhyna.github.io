29a4a9972073c68f6be249a1ca40125cbb2bf678
ValeriiaRohozhyna <rohozhyna.valeriia@gmail.com>
2020-04-02 19:51:40 +0300

    last changes

--- time/main.js
+++ time/main.js
@@ -1,14 +1,35 @@
+btnShow.addEventListener("click", showTime, false);
 
-    btnShow.addEventListener("click", showTime, false);
+function showTime() {
+    var timeNow = new Date();
 
-    function showTime () {
-        var timeNow = document.createElement("span");
-        timeNow = new Date();
-        var timeNowSec = timeNow.getSeconds();
-        var timeNowMin = timeNow.getMinutes();
-        var timeNowH = timeNow.getHours();
-        timeNow = `${timeNowH}:${timeNowMin}:${timeNowSec}`;
-        time.innerText = timeNow;
-
-        setTimeout(showTime, 1000);
+    var second = timeNow.getSeconds();
+    var minute = timeNow.getMinutes();
+    var hour = timeNow.getHours();
+    
+    if (hour.length < 2) {
+        hour = `0${hour}`;
     };
+    if (minute.length < 2) {
+        minute = `0${minute}`;
+    };
+    if (second.length < 2) {
+        second = `0${second}`;
+    };
+    timeNow = `${hour}:${minute}:${second}`;
+    time.innerText = timeNow;
+    setInterval(showTime, 1000);
+};
+
+btnShow.addEventListener("click", hideTime, false);
+
+function hideTime() {
+    if(btnShow.value != "Hide time"){
+        btnShow.value = "Hide time";
+        time.style.display = "block";
+        showTime();
+    } else {
+        btnShow.value = "Show time";
+        time.style.display = "none";
+    }
+};
